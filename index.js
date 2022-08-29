27061df290b400bd20e2a82f6103323e3d4ecaf2
Hunghh90 <hungbkvn9x1@gmail.com>
2022-08-29 19:00:43 +0700

    upcode

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =  require("express");
 const app = express();
-const port = process.env.Port | 6789;
+const port = process.env.Port | 5000;
 
 app.listen(port,function () {
     console.log("sever is running....");
